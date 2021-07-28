8d98a7ec113f1f5b613d1b1b1c429787f954c341
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 16:47:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main (String [] args){
+
+
+
+        mensch Mensch1 = new mensch("bob", 19, 45.7);
+        mensch Mensch2 = new mensch("tom", 20 , 68.3);
+
+        System.out.println(Mensch2.name);
+
+        Mensch2.drink();
+        Mensch1.eat();
+
+
+
+
+
+
+    }
+}
---
+++ src/mensch.java
@@ -0,0 +1,25 @@
+public class mensch {
+
+    String name;
+    int age;
+    double weight;
+
+
+
+    mensch (String name, int age, double weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+
+
+    }
+
+    void eat(){
+        System.out.println(this.name + " is eating");
+    }
+
+    void drink(){
+        System.out.println(this.name + " is drinking");
+    }
+
+}
